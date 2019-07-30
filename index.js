1f3ff8007db719d3af258ea4ea0b3dcd27c07136
MAKSYM_enko <finside41@gmail.com>
2019-07-30 21:46:25 +0300

    first commit options

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function() {
+  console.log(`App listening on port http://127.0.0.1:${port}`);
+});
