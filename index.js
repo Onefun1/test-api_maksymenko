59bb9ee8aa2a3315da98ef5feaa70d06538b5bd6
MAKSYM_enko <finside41@gmail.com>
2019-07-30 22:40:41 +0300

    add

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
+const users = [
+  { name: "Max", age: 30 },
+  { name: "Oksana", age: 29 },
+  { name: "Miroslava", age: 2 }
+];
+
+app.get("/api/users", function(req, res) {
+  res.send(JSON.stringify(users));
 });
 
 app.listen(port, function() {
